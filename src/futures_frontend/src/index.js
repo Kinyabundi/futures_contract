ad4a9718835f34ab22a9338fb457e403e1f794fb
Christine Kinya <kinyachristine14@gmail.com>
2024-02-18 01:35:17 +0300

    integrate internet identity

--- src/futures_frontend/src/index.js
+++ src/futures_frontend/src/index.js
@@ -1,19 +1,46 @@
-import { futures_backend } from "../../declarations/futures_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+//interner identity
+import {
+  createActor,
+  cropcontract_backend,
+} from "../../declarations/futures_backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { HttpAgent } from "@dfinity/agent";
+let actor = cropcontract_backend;
+console.log(process.env.CANISTER_ID_INTERNET_IDENTITY);
+
+
+const loginButton = document.getElementById("login");
+loginButton.onclick = async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
+  let authClient = await AuthClient.create();
+  // start the login process and wait for it to finish
+  await new Promise((resolve) => {
+    authClient.login({
+      identityProvider:
+        process.env.DFX_NETWORK === "ic"
+          ? "https://identity.ic0.app"
+          : `http:/bd3sg-teaaa-aaaaa-qaaba-cai/.localhost:4943`,
+      onSuccess: resolve,
+    });
+  });
+  const identity = authClient.getIdentity();
+  const agent = new HttpAgent({ identity });
+  actor = createActor(process.env.CANISTER_ID_CROPCONTRACT_BACKEND, {
+    agent,
+  });
+  
+  console.log(actor);
 
-  const name = document.getElementById("name").value.toString();
+  // Emit events
+  loginEmitter.emit('loginEvent', 'Login was successful!');
 
-  button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await futures_backend.greet(name);
+  // backend functions
+  // get_principal
+  console.log("Get principal:");
+  const get_principal = await actor.get_principal();
+  console.log(get_principal);
+  // document.getElementById("output_test").innerHTML = get_principal;
 
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+};
